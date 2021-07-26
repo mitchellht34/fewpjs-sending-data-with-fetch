c9fea5729cb5656aa41b7ffccd1e72a55f95cf8c
Mitch <mitchellht34@gmail.com>
2021-07-26 17:03:13 -0400

    commit tree f8df087ba094d8b160131aa29692789885350b3d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    const formData = {
+        name: userName,
+        email: userEmail
+    }
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            document.body.innerHTML = object.id;
+        })
+        .catch(function (error) {
+            document.body.innerHTML = error.message;
+        })
+}
